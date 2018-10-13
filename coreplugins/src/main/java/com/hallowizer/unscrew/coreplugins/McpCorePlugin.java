e3bcde188ee9a02029f32dba47e9c12c6d94d23b
Hallowizer <matthew@munge.com>
2018-10-12 22:21:53 -0700

    Enable deobfuscation on server launch.

--- coreplugins/src/main/java/com/hallowizer/unscrew/coreplugins/McpCorePlugin.java
+++ coreplugins/src/main/java/com/hallowizer/unscrew/coreplugins/McpCorePlugin.java
@@ -1,9 +1,15 @@
 package com.hallowizer.unscrew.coreplugins;
 
+import java.io.File;
+import java.util.List;
+
+import com.hallowizer.unscrew.api.CorePlugin;
 import com.hallowizer.unscrew.api.IClassTransformer;
 import com.hallowizer.unscrew.coreplugins.launcher.BuiltinCorePlugin;
 
+import cpw.mods.fml.relauncher.RelaunchClassLoader;
 import net.minecraftforge.fml.common.asm.transformers.DeobfuscationTransformer;
+import net.minecraftforge.fml.common.asm.transformers.deobf.FMLDeobfuscatingRemapper;
 
 public final class McpCorePlugin extends BuiltinCorePlugin {
 	@Override
@@ -16,6 +22,11 @@ public final class McpCorePlugin extends BuiltinCorePlugin {
 		return new IClassTransformer[] {
 			new DeobfuscationTransformer()
 		};
+	}
+	
+	@Override
+	public void injectData(List<CorePlugin> corePlugins, File location) {
+		FMLDeobfuscatingRemapper.INSTANCE.setup(null, (RelaunchClassLoader) getClass().getClassLoader(), "deobfuscation_data-1.12.2.lzma");
 	}
 	
 	@Override
